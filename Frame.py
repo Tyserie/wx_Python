0cb61b9c2e1bb0805f1acc9395f53cfd212341f1
Tyserie <tomas.tyser@seznam.cz>
2020-07-06 21:36:56 +0200

    "adding some code"

--- Frame.py
+++ Frame.py
@@ -2,9 +2,12 @@ import wx
 
 class windowClass(wx.Frame):
     def __init__(self, parent, title):
-        super(windowClass, self).__init__(parent, title=title, size =(500,300))
+        super(windowClass, self).__init__(parent, title=title,
+            size =(500,300))
 
+        self.Center()
+        #self.Move(800, 400)
         self.Show()
 app = wx.App()
-windowClass(None, title='TRUCKER Application')
+windowClass(None, title='LANNUTTI Czech - Trucker')
 app.MainLoop()
