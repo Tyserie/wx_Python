fdd20d8e0427d616e2c8378375365190cfffe270
Tyserie <tomas.tyser@seznam.cz>
2020-07-30 21:37:26 +0200

    .

---
+++ no_minimize.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+# no_minimize.py
+
+import wx
+
+app = wx.App()
+frame = wx.Frame(None, style=wx.MAXIMIZE_BOX | wx.RESIZE_BORDER
+                             | wx.SYSTEM_MENU | wx.CAPTION | wx.CLOSE_BOX)
+frame.Show(True)
+
+app.MainLoop()
